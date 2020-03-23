03968f400fd00ca6b2ec62275704089964171966
Mayuri Lahane <mlahane@redhat.com>
2020-03-23 20:14:00 +0530

    Added some examples related to try and except

---
+++ exception.py
@@ -0,0 +1,26 @@
+"""exception are of types -
+NameError
+TypeError
+KeyboardIntrurrpt,etc"""
+
+"""we can use try and except to handle this exception"""
+try:
+    print(abc)
+except: #only except can handle any type or exception
+    print("no value defined for abc")
+
+#finally:
+"""we can use finally block to execute those statments which needs to execute 
+at any condition"""
+finally:
+print("every file needs to close")
+fobj.close()
+
+#only this will give name error to handle this:
+try:
+    x = input(int("enter any no."))
+    print(abc)
+except Exception as e: #you can specify proper name of exception
+    print("no value defined")
+    print(e)
+    print(e)
